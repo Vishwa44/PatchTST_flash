e04c0e7665f3ffec0ec2cfcaca581d52927c6e2f
Vishwa Gohil <vishwa.gohil11@gmail.com>
2024-04-16 15:45:19 -0400

    Adding files

---
+++ get_params.py
@@ -0,0 +1,163 @@
+import argparse
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Autoformer & Transformer family for Time Series Forecasting')
+
+    # random seed
+    parser.add_argument('--random_seed', type=int, default=2021, help='random seed')
+
+    # basic config
+    parser.add_argument('--is_training', type=int, required=False, default=1, help='status')
+    parser.add_argument('--model_id', type=str, required=False, default='test', help='model id')
+    parser.add_argument('--model', type=str, required=False, default='Autoformer',
+                        help='model name, options: [Autoformer, Informer, Transformer]')
+
+    # data loader
+    parser.add_argument('--data', type=str, required=False, default='ETTm1', help='dataset type')
+    parser.add_argument('--root_path', type=str, default='./data/ETT/', help='root path of the data file')
+    parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='data file')
+    parser.add_argument('--features', type=str, default='M',
+                        help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, S:univariate predict univariate, MS:multivariate predict univariate')
+    parser.add_argument('--target', type=str, default='OT', help='target feature in S or MS task')
+    parser.add_argument('--freq', type=str, default='h',
+                        help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
+    parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
+
+    # forecasting task
+    parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
+    parser.add_argument('--label_len', type=int, default=48, help='start token length')
+    parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
+
+
+    # DLinear
+    #parser.add_argument('--individual', action='store_true', default=False, help='DLinear: a linear layer for each variate(channel) individually')
+
+    # PatchTST
+    parser.add_argument('--fc_dropout', type=float, default=0.05, help='fully connected dropout')
+    parser.add_argument('--head_dropout', type=float, default=0.0, help='head dropout')
+    parser.add_argument('--patch_len', type=int, default=16, help='patch length')
+    parser.add_argument('--stride', type=int, default=8, help='stride')
+    parser.add_argument('--padding_patch', default='end', help='None: None; end: padding on the end')
+    parser.add_argument('--revin', type=int, default=1, help='RevIN; True 1 False 0')
+    parser.add_argument('--affine', type=int, default=0, help='RevIN-affine; True 1 False 0')
+    parser.add_argument('--subtract_last', type=int, default=0, help='0: subtract mean; 1: subtract last')
+    parser.add_argument('--decomposition', type=int, default=0, help='decomposition; True 1 False 0')
+    parser.add_argument('--kernel_size', type=int, default=25, help='decomposition-kernel')
+    parser.add_argument('--individual', type=int, default=0, help='individual head; True 1 False 0')
+
+    # Formers
+    parser.add_argument('--embed_type', type=int, default=0, help='0: default 1: value embedding + temporal embedding + positional embedding 2: value embedding + temporal embedding 3: value embedding + positional embedding 4: value embedding')
+    parser.add_argument('--enc_in', type=int, default=7, help='encoder input size') # DLinear with --individual, use this hyperparameter as the number of channels
+    parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
+    parser.add_argument('--c_out', type=int, default=7, help='output size')
+    parser.add_argument('--d_model', type=int, default=512, help='dimension of model')
+    parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
+    parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
+    parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
+    parser.add_argument('--d_ff', type=int, default=2048, help='dimension of fcn')
+    parser.add_argument('--moving_avg', type=int, default=25, help='window size of moving average')
+    parser.add_argument('--factor', type=int, default=1, help='attn factor')
+    parser.add_argument('--distil', action='store_false',
+                        help='whether to use distilling in encoder, using this argument means not using distilling',
+                        default=True)
+    parser.add_argument('--dropout', type=float, default=0.05, help='dropout')
+    parser.add_argument('--embed', type=str, default='timeF',
+                        help='time features encoding, options:[timeF, fixed, learned]')
+    parser.add_argument('--activation', type=str, default='gelu', help='activation')
+    parser.add_argument('--output_attention', action='store_true', help='whether to output attention in ecoder')
+    parser.add_argument('--do_predict', action='store_true', help='whether to predict unseen future data')
+
+    # optimization
+    parser.add_argument('--num_workers', type=int, default=10, help='data loader num workers')
+    parser.add_argument('--itr', type=int, default=2, help='experiments times')
+    parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
+    parser.add_argument('--batch_size', type=int, default=128, help='batch size of train input data')
+    parser.add_argument('--patience', type=int, default=100, help='early stopping patience')
+    parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
+    parser.add_argument('--des', type=str, default='test', help='exp description')
+    parser.add_argument('--loss', type=str, default='mse', help='loss function')
+    parser.add_argument('--lradj', type=str, default='type3', help='adjust learning rate')
+    parser.add_argument('--pct_start', type=float, default=0.3, help='pct_start')
+    parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
+
+    # GPU
+    parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
+    parser.add_argument('--gpu', type=int, default=0, help='gpu')
+    parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
+    parser.add_argument('--devices', type=str, default='0,1,2,3', help='device ids of multile gpus')
+    parser.add_argument('--test_flop', action='store_true', default=False, help='See utils/tools for usage')
+    parser.add_argument('--profiler_logdir',  default="log/profiler_dir", help='profiler log dir')
+    parser.add_argument('--use_flashattention', action='store_true', default=False, help='use flash attention')
+    args = parser.parse_args()
+
+    print(args)
+    for i in args:
+        print(i)
+
+
+
+
+"""
+python .\PatchTST_supervised\get_params.py --random_seed 2021 --is_training 1 --root_path name --data_path name --model_id model_id --model PatchTST --data custom --features M --seq_len 336 --pred_len 96 --enc_in 21 --e_layers 3 --n_heads 16 --d_model 128 --d_ff 256 --dropout 0.2 --fc_dropout 0.2 --head_dropout 0 --patch_len 16 --stride 8 --des 'Exp'  --train_epochs 100 --patience 20 --itr 1 --batch_size 128 --learning_rate 0.0001 
+"""
+
+
+activation='gelu'
+affine=0
+batch_size=128
+c_out=7
+checkpoints='./checkpoints/'
+d_ff=256
+d_layers=1
+d_model=128
+data='custom'
+data_path='name'
+dec_in=7
+decomposition=0
+des='Exp'
+devices='0,1,2,3'
+distil=True
+do_predict=False
+dropout=0.2
+e_layers=3
+embed='timeF'
+embed_type=0
+enc_in=21
+factor=1
+fc_dropout=0.2
+features='M'
+freq='h'
+gpu=0
+head_dropout=0.0
+individual=0
+is_training=1
+itr=1
+kernel_size=25
+label_len=48
+learning_rate=0.0001
+loss='mse'
+lradj='type3'
+model='PatchTST'
+model_id='model_id'
+moving_avg=25
+n_heads=16
+num_workers=10
+output_attention=False
+padding_patch='end'
+patch_len=16
+patience=20
+pct_start=0.3
+pred_len=96
+profiler_logdir='log/profiler_dir'
+random_seed=2021
+revin=1
+root_path='name'
+seq_len=336
+stride=8
+subtract_last=0
+target='OT'
+test_flop=False
+train_epochs=100
+use_amp=False
+use_flashattention=False
+use_gpu=True
+use_multi_gpu=False
